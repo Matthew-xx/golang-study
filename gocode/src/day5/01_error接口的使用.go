94e3dc0dbc209939cec25b329882e87caa27861c
Lerix <185734549@qq.com>
2019-05-30 10:39:45 +0800

    fix

--- gocode/src/day5/01_error接口的使用.go
+++ gocode/src/day5/01_error接口的使用.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println(e)
 	//e2 := fmt.Errorf("%s", "这是两个自己的错误")
 	e2 := fmt.Errorf("test:%s", "fmt：错误")
-	fmt.Println(e2)
+	fmt.Println(e2) //ewrrrrrr
 	var e3 error
 	//e3 = fmt.Errorf("%s", e)
 	fmt.Println(e3)
